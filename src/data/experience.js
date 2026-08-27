d9d0ab3c5a01ff1dfde8441c10e6a47a23a264b9
irffann-git <rxmeirfan7@gmail.com>
2026-08-27 12:33:03 +0530

    nscdd

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,12 +1,13 @@
 const experiences = [
   
-   {
-    title: "Backend Developer",
-    company: "Self Learning",
-    date: "03/2026 - Present",
-    desc: "Building scalable backend applications using Node.js, Express.js, MongoDB, REST APIs, authentication systems, and database management.",
-    icon: "fas fa-database"
-  },
+
+  {
+  title: "MERN Stack Trainee",
+  company: "SysBreeze Technologies, Malappuram",
+  date: "04/2026 - Present",
+  desc: "Working as a MERN Stack Trainee, developing full-stack web applications using MongoDB, Express.js, React.js, and Node.js. Building REST APIs, implementing authentication and database management, developing responsive user interfaces, and gaining hands-on experience with real-world application development.",
+  icon: "fas fa-code"
+},
   {
     title: "Shopify Frontend Developer",
     company: "Seamedia E-Commerce Solutions",
